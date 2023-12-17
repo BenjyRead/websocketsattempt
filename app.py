559db3cff6bc529fd8d779ef780abd49c9748713
benjamin read <readb5@cardiff.ac.uk>
2023-12-17 14:40:04 +0000

    finished base I think

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-import flask
+from flask import Flask, redirect, send_from_directory
+
+app = Flask(__name__)
+
+@app.get('/')
+def index():
+    return redirect('/home')
+
+@app.get('/home')
+def home():
+    return send_from_directory('static', 'home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
